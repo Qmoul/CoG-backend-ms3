339a9163c68ebe08074802a3be1024314273a175
Molnár Tamás <qmouls@gmail.com>
2020-05-08 05:51:47 +0200

    feat: Created the User_party join model, and the corresponding API

---
+++ models/User_party.js
@@ -0,0 +1,29 @@
+const sequelize = require('sequelize');
+const db = require('../config/db');
+
+const Party = require('../models/Party');
+const User = require('../models/Party');
+
+const User_party = db.define('user_party', {
+  user_id: {
+    type: sequelize.INTEGER,
+    reference: {
+        model:'users',
+        key:'id'
+    }
+  },
+  party_id: {
+      type: sequelize.INTEGER,
+    reference : {
+        model:'activities',
+        key:'id'
+    }
+      }, 
+    numberofusers: sequelize.INTEGER,
+  isgoing: sequelize.BOOLEAN
+});
+
+User.hasMany(User_party);
+Party.hasMany(User_party);
+
+module.exports = User_party;
--- restapi/parties.js
+++ restapi/parties.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../middleware/auth');
 
-const User = require('../models/User');
 const Activity = require('../models/Activity');
 const Party = require('../models/Party');
+const User_party = require('../models/User_party');
 
 // @route   POST restapi/parties
 // @desc    Create a post
@@ -22,7 +22,7 @@ router.post(
         return res.status(400).json({ errors: errors.array() });
       }
       try {
-        const user = await User.findByPk(req.user.id);
+          //Creating the party itself
         const activity = await Activity.findOne({where: {name: req.body.activity_name}})
         const newParty = new Party({
           name: req.body.name,
@@ -31,7 +31,15 @@ router.post(
           activity_id: activity.id,
           isagroup: false
         });
+        //Creat the join table data, automatically adding 1 to the number of users
         const party = await newParty.save();
+        const newUserParty = new User_party({
+            user_id: req.user.id,
+            party_id: party.id,
+            numberofusers: 1,
+            isgoing: 0,
+        })
+        await newUserParty.save();
         res.json(party);
       } catch (err) {
         console.error(err.message);
